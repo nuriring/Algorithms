71a67f47f30f387131bb303c02a89c8fad675827
unknown <zsda3131@naver.com>
2023-02-11 00:14:29 +0900

    .

--- 1일1알고/구현/0210/14500_테트로미노.py
+++ 1일1알고/구현/0210/14500_테트로미노.py
@@ -24,19 +24,37 @@ def dfs(x,y,ssum, cnt):
 
 
 
-# 산 모양 최대값
-di = [0,0,-1]
-dj = [1,-1,0]
-def mount(x,y):
+
+def mount(x,y,ssum):
+    global mmax
+    tmp = ssum
+    for n in range(4):
+
+        ssum = tmp
+
+        for k in range(3):
+            #012 123 230 301
+            t = (n+k)%4
+            nx = x + dx[t]
+            ny = y + dy[t]
+            if 0 <= nx < N and 0 <= ny < M:
+                ssum+= arr[nx][ny]
+        mmax = max(mmax, ssum)
+
+
 
 
 
 N,M = map(int,input().split()) #N이 세로 #M이가로
 arr = [list(map(int,input().split())) for _ in range(N)]
 visited = [[0]*M for _ in range(N)]
-print(arr)
+#print(arr)
 
 mmax = 0
-dfs(0,0,arr[0][0],1)
+for i in range(N):
+    for j in range(M):
+        visited[i][j] = 1
+        dfs(i,j,arr[i][j],1)
+        visited[i][j] = 0
+        mount(i,j,arr[i][j])
 print(mmax)
-print(visited)
